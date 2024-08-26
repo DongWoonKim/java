525be2ade732e8bb2fb11de4a2b8cdadfaf76061
DongwoonKim <01030985843z@naver.com>
2024-08-26 14:55:13 +0900

    ~회원관리 프로그램

--- java-part-1/src/com/himedia/java/K_member_management.java
+++ java-part-1/src/com/himedia/java/K_member_management.java
@@ -4,6 +4,9 @@ import java.util.Scanner;
 
 public class K_member_management {
 
+    static int totalCnt = 0;
+    static int totalMemberCnt = 0;
+
     // 요금제
     public static int printPricePlan() {
         Scanner sc = new Scanner(System.in);
@@ -15,12 +18,28 @@ public class K_member_management {
 
     public static int printMenu() {
         Scanner sc = new Scanner(System.in);
-        System.out.println("[수행할 업무를 선택하세요]");
+        System.out.println("[수행할 업무를 선택하세요 - 현재 회원수 " + totalMemberCnt + " / " + totalCnt + "]");
         System.out.println("[1]회원추가 [2]회원조회(메일) [3]회원조회(이름)");
         System.out.println("[4]전체조회 [5]회원정보 수정 [6]회원삭제");
         System.out.println("[7]프로그램 종료");
 
         return sc.nextInt();
+    }
+
+    public static void addMember(String[][] members) {
+        // 사용자로부터 이름, 이메일, 연락처
+        Scanner sc = new Scanner(System.in);
+        System.out.println("이름을 입력하세요.");
+        String name = sc.nextLine();
+        System.out.println("이메일을 입력하세요.");
+        String email = sc.nextLine();
+        System.out.println("연락처를 입력하세요.");
+        String phone = sc.nextLine();
+
+        // members에 넣어주세요.
+        members[totalMemberCnt][0] = name;
+        // 이메일, 연락처...
+
     }
 
     public static void main(String[] args) {
@@ -30,10 +49,27 @@ public class K_member_management {
         // 배열명 : members
         int pricePlanNum = printPricePlan();
         String[][] members = new String[pricePlanNum * 10][3];
+        totalCnt = pricePlanNum * 10;
 
         while (true) {
             // 7번 프로그램 종료를 완성시켜주세요.
             // "이용해주셔서 감사합니다."
+            int menuNum = printMenu();
+            switch (menuNum) {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 7:
+                    System.out.println("이용해주셔서 감사합니다.");
+                    return;
+
+                default:
+                    System.out.println("잘 못 선택하셨습니다.");
+                    break;
+            }
         }
 
     }

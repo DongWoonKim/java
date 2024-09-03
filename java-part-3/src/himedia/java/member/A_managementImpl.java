83cb4b948a5f8ef305b2c50f0a1481dea6d39555
DongwoonKim <01030985843z@naver.com>
2024-09-03 16:56:10 +0900

    collection 과제

--- java-part-3/src/himedia/java/member/A_managementImpl.java
+++ java-part-3/src/himedia/java/member/A_managementImpl.java
@@ -6,6 +6,25 @@ import java.util.Scanner;
 
 import static himedia.java.member.A_PricePlan.*;
 
+/*
+[멤버 관리 프로그램]
+->Member.java(I)
+->ManagementImpl.java // 필드 -> private
+->Start.java
+
+// 반드시 Collections중 하나를 적용할 것.
+[1]Lite : 10명 [2]Basic : 20명 [3]Premium : 30명 [4]Freepass : 무제한
+
+요금제 관리 -> enum
+
+[1]회원추가 [2]회원조회(메일) [3]회원조회(이름)
+[4]전체조회 [5]회원정보 수정 [6]회원삭제
+[7]요금제 변경 [8]초기화 [9]프로그램 종료
+
+// 10명... -> 꽉 차면 "요금제를 변경하시오."
+
+ */
+
 public class A_managementImpl implements A_member {
 
     private A_PricePlan pricePlan;

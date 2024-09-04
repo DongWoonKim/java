185e8587dd9b70e98cc25992a925571d70736a92
DongwoonKim <01030985843z@naver.com>
2024-09-04 11:54:43 +0900

    ~queue bfs

--- java-part-3/src/himedia/java/A_collections_queue.java
+++ java-part-3/src/himedia/java/A_collections_queue.java
@@ -38,6 +38,29 @@ class A_person {
     }
 }
 
+class Graph {
+    private LinkedList<Integer>[] graphList;
+
+    public Graph(int vertexCount) {
+        this.graphList = new LinkedList[vertexCount + 1];
+        
+        // 각 인접리스트 초기화
+        for (int i = 0; i < graphList.length; i++) {
+            graphList[i] = new LinkedList<>();
+        }
+    }
+    
+    // 간선 추가
+    public void addEdge(int v, int w) {
+        graphList[v].add(w);
+        graphList[w].add(v);
+    }
+
+    public LinkedList<Integer>[] getGraphList() {
+        return graphList;
+    }
+}
+
 public class A_collections_queue {
 
     // 1. LinkedList
@@ -240,7 +263,46 @@ public class A_collections_queue {
 
     }
 
+    public static void exam4() {
+        boolean[] visited = new boolean[9 + 1];
+
+        Graph graph = new Graph(9);
+        graph.addEdge(1, 2);
+        graph.addEdge(1, 3);
+        graph.addEdge(2, 3);
+        graph.addEdge(2, 4);
+        graph.addEdge(2, 6);
+        graph.addEdge(3, 7);
+        graph.addEdge(4, 5);
+        graph.addEdge(4, 7);
+        graph.addEdge(4, 8);
+        graph.addEdge(5, 6);
+        graph.addEdge(7, 8);
+        graph.addEdge(8, 9);
+
+        // 탐색 -> BFS
+        int startVertex = 1;
+        Queue<Integer> queue = new LinkedList<>();
+        visited[startVertex] = true;
+        queue.add(startVertex);
+
+        while (queue.size() > 0) {
+            int vertex = queue.poll();
+            System.out.print( vertex + " -> " );
+            
+            // 현재 정점의 인접 정점들 중 방분하지 않은 정점을 모두 큐에 추가
+            for (int w : graph.getGraphList()[vertex]) {
+                if (!visited[w]) {
+                    queue.add(w);
+                    visited[w] = true;
+                }
+            }
+            
+        }
+
+    }
+
     public static void main(String[] args) {
-        exam2_5();
+        exam4();
     }
 }

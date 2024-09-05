554453b86305fc59b65d33ea1c768f72339c73da
DongwoonKim <01030985843z@naver.com>
2024-09-05 11:38:02 +0900

    ~thread7

--- java-part-3/src/himedia/java/C_thread_7.java
+++ java-part-3/src/himedia/java/C_thread_7.java
@@ -80,7 +80,61 @@ class ThreadEx_interrupt_1 extends Thread {
 class ThreadEx_interrupt_2 extends Thread {
     @Override
     public void run() {
+        int i = 10;
+        while ( i != 0 && !isInterrupted() ) {
+            System.out.println(i--);
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
 
+        System.out.println("카운트가 종료되었습니다.");
+    }
+}
+
+class ThreadEx_yield extends Thread {
+    private String name;
+
+    public ThreadEx_yield(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 5; ++i) {
+            System.out.println(name + " is running. Iteration : " + i);
+
+            // 현재 실행 중인 스레드가 다른 스레드에게 실행 기회를 양보
+            Thread.yield();
+
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
+
+class ThreadEx_join_1 extends Thread {
+    @Override
+    public void run() {
+        for (int i = 0; i < 30000; ++i) {
+            System.out.print("-");
+        }
+        System.out.println();
+    }
+}
+
+class ThreadEx_join_2 extends Thread {
+    @Override
+    public void run() {
+        for (int i = 0; i < 30000; ++i) {
+            System.out.print("|");
+        }
+        System.out.println();
     }
 }
 
@@ -111,7 +165,12 @@ public class C_thread_7 {
 
     // - interrupt() : 스레드의 작업을 취소한다.
     // 진행중인 스레드의 작업이 끝나기 전에 취소시켜야할 때가 있다.
-    // 
+    // 예를들어 큰 파일을 다운로드 받을 때 시간이 너무 오래 걸리면 중간에 다운로드를 포기하고
+    // 취소할 수 있어야 한다.
+    // interrupt()는 스레드에게 작업을 멈추라고 요청한다.
+    // 단지 멈추라고 요청만 하는 것일 뿐 스레드를 강제로 종료시키지는 못한다.
+    // 그리고 interrupted()는 스레드에 대해 interrupt()가 호출되었는지 알려준다.
+    // interrupted()가 호출되지 않았다면 false, 호출되었다면 true를 반환한다.
     public static void exam2_1() {
         ThreadEx_interrupt_1 t1 = new ThreadEx_interrupt_1();
         t1.start();
@@ -125,7 +184,59 @@ public class C_thread_7 {
         System.out.println("I2 : " + t1.isInterrupted());
     }
 
+    // 스레드가 sleep(), wait(), join()에 의해 '일시 정지 상태(WAITING)'에 있을 때,
+    // 해당 스레드에 대해 interrupt()를 호출하면, sleep(), wait(), join()에서
+    // InterruptedException이 발생하고 스레드는 '실행 대기 상태'로 바뀐다.
+    public static void exam2_2() {
+        ThreadEx_interrupt_2 t1 = new ThreadEx_interrupt_2();
+        t1.start();
+
+        System.out.println("I1 : " + t1.isInterrupted());
+        System.out.println("아무 값이나 입력하세요.");
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        System.out.println("입력한 값은 " + input + "입니다.");
+        t1.interrupt();
+        System.out.println("I2 : " + t1.isInterrupted());
+    }
+
+    // - Thread.yield()
+    // 자바의 스레드 제어 메서드 중 하나로, 현재 실행 중인 스레드가 CPU를 다른 스레드에게 양보하도록한다.
+    // 즉 실행 상태에 있는 스레드가 자신의 남은 시간을 포기하고 다른 스레드에게 기회를 주도록
+    // 스케줄러에게 신호를 보내는 역할을 한다.
+    // 그러나 yield() 메서드는 단순히 힌트일 뿐이며, 반드시 다른 스레드가 CPU시간을 받는다는 보장은 없다.
+    // 실제로 CPU를 다른 스레드가 차지할지 여부는 자바 스레드 스케줄러와 운영체제의 결정에 따라 달라진다.
+    public static void exam3() {
+        ThreadEx_yield t1 = new ThreadEx_yield("Thread 1");
+        ThreadEx_yield t2 = new ThreadEx_yield("Thread 2");
+
+        t1.start();
+        t2.start();
+    }
+
+    // - join() : 다른 스레드의 작업을 기다린다.
+    public static void exam4() {
+        ThreadEx_join_1 t1 = new ThreadEx_join_1();
+        ThreadEx_join_2 t2 = new ThreadEx_join_2();
+
+        t1.start();
+        t2.start();
+
+        long startTime = System.currentTimeMillis();
+
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("소요시간 : " + (System.currentTimeMillis() - startTime) + "ms");
+        // join을 사용하지 않았다면 main스레드가 바로 종료되었겠지만,
+        // join()으로 스레드 t1, t2가 모두 작업을 마칠 때까지 main스레드가 기다리도록 하였다.
+    }
+
     public static void main(String[] args) {
-        exam2_1();
+        exam4();
     }
 }

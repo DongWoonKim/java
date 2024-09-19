07d9c6941b6a870a43edb194e950c2eb19a4b6cc
DongwoonKim <01030985843z@naver.com>
2024-09-19 11:00:43 +0900

    tobi spring start

---
+++ spring-tobi/src/main/java/com/example/tobi/springtobi/SpringTobiApplication.java
@@ -0,0 +1,43 @@
+package com.example.tobi.springtobi;
+
+import com.example.tobi.springtobi.dao.UserDao;
+import com.example.tobi.springtobi.domain.User;
+
+import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
+import java.sql.SQLException;
+
+public class SpringTobiApplication {
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        setCharacter();
+
+        UserDao dao = new UserDao();
+
+        User user = new User();
+        user.setId("tobi");
+        user.setName("Tobi");
+        user.setPassword("1234");
+
+        dao.add(user);
+
+        System.out.println(user.getId() + " 등록성공!");
+
+        User getUser = dao.get(user.getId());
+
+        System.out.println(getUser.getName());
+        System.out.println(getUser.getPassword());
+
+        System.out.println(getUser.getId() + " 조회 성공");
+
+    }
+
+    private static void setCharacter() {
+        try {
+            System.setOut(new PrintStream(System.out, true, "UTF-8"));
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ spring-tobi/src/test/java/com/example/tobi/springtobi/SpringTobiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tobi.springtobi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTobiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

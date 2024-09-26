b8d393cd9b577486bb13436dd096b3b77c97565e
DongwoonKim <01030985843z@naver.com>
2024-09-26 15:54:18 +0900

    ~set up

--- spring-boot-basic/src/main/java/com/example/tobi/springbootbasic/controller/HelloController.java
+++ spring-boot-basic/src/main/java/com/example/tobi/springbootbasic/controller/HelloController.java
@@ -3,6 +3,13 @@ package com.example.tobi.springbootbasic.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+/*
+- url요청 방식
+GET, POST, PUT, DELETE
+- MVC패턴?
+- 템플릿엔진?
+ */
+
 @Controller
 public class HelloController {
 

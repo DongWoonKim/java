2550fe18f78d5f03976ee3674a16fd3bcb9c5e55
DongwoonKim <01030985843z@naver.com>
2024-09-27 11:52:47 +0900

    springboot v1 list

--- spring-boot-basic/src/main/java/com/example/tobi/springbootbasic/controller/UserController.java
+++ spring-boot-basic/src/main/java/com/example/tobi/springbootbasic/controller/UserController.java
@@ -4,6 +4,7 @@ import com.example.tobi.springbootbasic.dto.MemberListResponseDTO;
 import com.example.tobi.springbootbasic.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -17,8 +18,10 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping
-    public String findAllUsers() {
+    public String findAllUsers(Model model) {
         List<MemberListResponseDTO> users = userService.findAll();
+        System.out.println(users.size());
+        model.addAttribute("users", users);
         return "user-list";
     }
 

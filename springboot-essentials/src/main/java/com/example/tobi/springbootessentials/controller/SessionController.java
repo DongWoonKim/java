01cf657b3e3cb2220ce5e624d66cfc9d8c70cf1a
DongwoonKim <01030985843z@naver.com>
2024-10-01 10:39:04 +0900

    springboot-essential cookie

--- springboot-essentials/src/main/java/com/example/tobi/springbootessentials/controller/SessionController.java
+++ springboot-essentials/src/main/java/com/example/tobi/springbootessentials/controller/SessionController.java
@@ -1,10 +1,45 @@
 package com.example.tobi.springbootessentials.controller;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class SessionController {
 
+    // 로그인 페이지
+    @GetMapping("/login")
+    public String login(
+            HttpSession session,
+            Model model
+    ) {
+        String username = (String) session.getAttribute("username");
+
+        if (username != null) {
+            model.addAttribute("username", username);
+        }
+
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String loginExc(
+            @RequestParam String username,
+            HttpSession session
+    ) {
+        session.setAttribute("username", username);
+        return "redirect:/login";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpSession session) {
+        //세션 무효화
+        session.invalidate();
+        return "redirect:/login";
+    }
 
 
 }

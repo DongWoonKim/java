a9c90359b484c39aafd8d3e9586043f45ea45757
DongwoonKim <01030985843z@naver.com>
2024-10-04 11:03:01 +0900

    springboot-basic-board

---
+++ springboot-basic-board/src/main/java/com/example/tobi/springbootbasicboard/SpringbootBasicBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.tobi.springbootbasicboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBasicBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootBasicBoardApplication.class, args);
+    }
+
+}
---
+++ springboot-basic-board/src/test/java/com/example/tobi/springbootbasicboard/SpringbootBasicBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tobi.springbootbasicboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBasicBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

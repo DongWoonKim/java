9af3beca4bac1ced8e0e5b0476373b09f0b78922
DongwoonKim <01030985843z@naver.com>
2024-10-10 09:45:11 +0900

    springboot-basic-board

---
+++ springboot-basic-board/src/main/java/com/example/tobi/springbootbasicboard/config/WebSecurityConfig.java
@@ -0,0 +1,48 @@
+package com.example.tobi.springbootbasicboard.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+public class WebSecurityConfig {
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring()
+                .requestMatchers(
+                        "/static/**", "/css/**", "/js/**"
+                );
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(
+                        auth -> auth
+                                .requestMatchers(
+                                        new AntPathRequestMatcher("/member/login"),
+                                        new AntPathRequestMatcher("/member/join"),
+                                        new AntPathRequestMatcher("/join")
+                                ).permitAll()
+                                .anyRequest().authenticated()
+                )
+                .formLogin(
+                        form -> form.loginPage("/member/login")
+                                .defaultSuccessUrl("/")
+                )
+                .logout(
+                        logout -> logout
+                                .logoutUrl("/logout")
+                                .logoutSuccessUrl("/member/login")
+                )
+                .csrf(AbstractHttpConfigurer::disable);
+
+        return http.build();
+    }
+
+}

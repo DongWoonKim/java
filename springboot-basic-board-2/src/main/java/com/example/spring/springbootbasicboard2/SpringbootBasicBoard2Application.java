6886c2618919d1b5b21968a17696888d4b37012d
DongwoonKim <01030985843z@naver.com>
2024-11-08 11:56:27 +0900

    board2 set up

---
+++ springboot-basic-board-2/src/main/java/com/example/spring/springbootbasicboard2/SpringbootBasicBoard2Application.java
@@ -0,0 +1,13 @@
+package com.example.spring.springbootbasicboard2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBasicBoard2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootBasicBoard2Application.class, args);
+    }
+
+}
---
+++ springboot-basic-board-2/src/test/java/com/example/spring/springbootbasicboard2/SpringbootBasicBoard2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring.springbootbasicboard2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBasicBoard2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

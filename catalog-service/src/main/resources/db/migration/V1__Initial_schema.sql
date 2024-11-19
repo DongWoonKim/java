77ea4d43a8d9debe62863d0f27ca134fcf031b59
DongwoonKim <01030985843z@naver.com>
2024-11-19 10:44:20 +0900

    flyway

---
+++ catalog-service/src/main/resources/db/migration/V1__Initial_schema.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE IF NOT EXISTS catalog
+    DEFAULT CHARACTER SET utf8mb4
+    DEFAULT COLLATE utf8mb4_unicode_ci;
+
+CREATE TABLE IF NOT EXISTS book (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    author VARCHAR(255) NOT NULL,
+    isbn VARCHAR(255) NOT NULL,
+    price DOUBLE NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    create_at TIMESTAMP NOT NULL,
+    last_modified_at TIMESTAMP NOT NULL,
+    version INT NOT NULL
+) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;

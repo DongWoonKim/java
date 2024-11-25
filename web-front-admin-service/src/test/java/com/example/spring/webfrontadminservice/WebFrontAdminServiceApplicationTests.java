8f8f26bbc4724f0dc7e776e3dac78586e198874d
DongwoonKim <01030985843z@naver.com>
2024-11-25 10:42:35 +0900

    web front admin

---
+++ web-front-admin-service/src/test/java/com/example/spring/webfrontadminservice/WebFrontAdminServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring.webfrontadminservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebFrontAdminServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

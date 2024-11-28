ca9bd5eadec52d37bf6a018dc68552a7415c39b8
DongwoonKim <01030985843z@naver.com>
2024-11-28 12:26:26 +0900

    update code

--- web-front-service/src/main/java/com/example/spring/webfrontservice/dto/JoinRequestDTO.java
+++ web-front-service/src/main/java/com/example/spring/webfrontservice/dto/JoinRequestDTO.java
@@ -1,5 +1,6 @@
 package com.example.spring.webfrontservice.dto;
 
+import com.example.spring.webfrontservice.enums.Role;
 import lombok.Getter;
 
 @Getter
@@ -7,5 +8,5 @@ public class JoinRequestDTO {
     private String userId;
     private String password;
     private String userName;
-
+    private Role role;
 }

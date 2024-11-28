9128a186fb57012f8bc3723715c0d427a6f57b15
DongwoonKim <01030985843z@naver.com>
2024-11-28 11:20:37 +0900

    web-front-service set up

---
+++ web-front-service/src/main/java/com/example/spring/webfrontservice/WebFrontServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring.webfrontservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebFrontServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebFrontServiceApplication.class, args);
+    }
+
+}
---
+++ web-front-service/src/test/java/com/example/spring/webfrontservice/WebFrontServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring.webfrontservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebFrontServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
